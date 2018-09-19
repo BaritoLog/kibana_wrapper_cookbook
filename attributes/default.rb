1c41f8a0de80c16678fec4322a38374edd644944
Giovanni Sakti <giosakti@gmail.com>
2018-09-19 15:45:33 +0700

    make kibana version overrideable

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['kibana']['config']['elasticsearch.url'] = 'http://el.baritolog.com:9200'
 override['kibana']['config']['base_dir'] = '/opt/kibana'
 override['kibana']['config']['logging.dest'] = '/var/log/kibana.log'
-override['kibana']['version'] = '5.6.9'
+default['kibana']['version'] = '5.6.9'
 default['nginx']['default_site_enabled'] = false
 default['nginx']['user'] = node['kibana']['service_user']
 default['nginx']['kibana_path'] = '/etc/nginx/conf.d/kibana.conf'
